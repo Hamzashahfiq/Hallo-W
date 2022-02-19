e5ae4dd58391c4fdfa3d4c58a35b911ca6558b2d
Muhammad hamza <hamzashafique535@gmail.com>
2022-02-19 10:54:32 +0500

    first commit for this project

---
+++ js/script.js
@@ -0,0 +1,9 @@
+balertname.onclick=function(){
+    var myName = "Muhammad Hamza"
+    alert(myName);
+   origanalstatement.innerHTML="var myName = Muhammad Hamza <br/> alert(myName);"
+}
+// balertnum.onclick=function(){
+//     alert(2);
+//     Origanalstatement.innerHTML="alert('Muhammad Hamza');"
+// }
